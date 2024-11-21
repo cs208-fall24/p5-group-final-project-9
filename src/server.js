4e113da7ba22c9f13b17f903b9fe1322520d3d87
Davina <davinacausey@u.boisestate.edu>
2024-11-21 03:42:47 -0700

    Stand up meeting 2

--- src/server.js
+++ src/server.js
@@ -6,11 +6,19 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
+// Code from Davina
+db.run(`CREATE TABLE IF NOT EXISTS s2_comments (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    submission TEXT NOT NULL
+  )
+`);
+
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
 app.set('view engine', 'pug')
 app.use(express.urlencoded({ extended: false }))
+
 
 app.get('/', function (req, res) {
   console.log('GET called')
